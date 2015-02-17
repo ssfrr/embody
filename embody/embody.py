d679a31e465c0320a0dccd9e35b4410d09bbcef5
Spencer Russell <spencer.f.russell@gmail.com>
2015-02-16 22:36:29 -0500

    switches to using pycparser for the codegen

--- embody/embody.py
+++ embody/embody.py
@@ -1,37 +1,23 @@
 from jinja2 import Environment, PackageLoader
-from clang.cindex import TranslationUnit, CursorKind
-import clang.cindex
+from pycparser import parse_file, c_ast
+from pycparser.c_generator import CGenerator
 
 
-class FuncProto:
-    def __init__(self, name, ret_type, args):
-        self.name = name
-        self.ret_type = ret_type
-        # args is a list of ('type', 'name') tuples
-        self.args = args
+class FuncDeclVisitor(c_ast.NodeVisitor):
+    def __init__(self):
+        self.stubs = []
 
-    @classmethod
-    def from_cursor(cls, cursor):
-        ret_type = None
-        args = []
-        name = ""
-        if cursor.kind != CursorKind.FUNCTION_DECL:
-            raise ValueError(
-                "Cursor kind was %s instead of CursorKind.FUNCTION_DECL"
-                % cursor.kind)
-        name = cursor.spelling
-        for child in cursor.get_children():
-            if child.kind == CursorKind.TYPE_REF:
-                ret_type = child.displayname
-            elif child.kind == CursorKind.PARM_DECL:
-                args.append((child.type.spelling, child.spelling))
-        if ret_type is None:
-            ret_type = 'void'
-        return FuncProto(name, ret_type, args)
+    def visit_FuncDecl(self, node):
+        gen = CGenerator()
+        self.stubs.append(gen.visit(node))
 
 
 def generate_fake(in_filename, fake_src_filename,
                   fake_header_filename):
+    ast = parse_file(in_filename, use_cpp=True)
+    v = FuncDeclVisitor()
+    v.visit(ast)
+
     env = Environment(loader=PackageLoader('embody', 'templates'))
     with open(fake_header_filename, 'w') as fake_header:
         fake_header_template = env.get_template('fake.h')
@@ -42,21 +28,9 @@ def generate_fake(in_filename, fake_src_filename,
         fake_header.write(fake_header_template.render(**ctx))
 
     with open(fake_src_filename, 'w') as fake_src:
-        funcs = get_funcs(in_filename)
         fake_src_template = env.get_template('fake.c')
         ctx = {
             'fake_include': fake_header_filename,
-            'funcs': funcs
+            'funcs': v.stubs
         }
         fake_src.write(fake_src_template.render(**ctx))
-
-
-def get_funcs(header_filename):
-    clang.cindex.Config.set_library_path(
-        '/Library/Developer/CommandLineTools/usr/lib/')
-    tu = TranslationUnit.from_source(header_filename)
-    funcs = []
-    for node in tu.cursor.get_children():
-        if node.kind == CursorKind.FUNCTION_DECL:
-            funcs.append(FuncProto.from_cursor(node))
-    return funcs
--- embody/templates/fake.c
+++ embody/templates/fake.c
@@ -1,11 +1,7 @@
 #include "{{ fake_include }}"
 
-{% for f in funcs %}
-{{ f.ret_type }} {{ f.name }}(
-    {%- for a in f.args -%}
-        {{ a[0] }}{% if a[0][-1] != '*' %} {% endif %}{{ a[1] }}
-        {%- if not loop.last %}, {% endif %}
-    {%- endfor %}) {
+{% for func in funcs %}
+{{ func }} {
 }{% if not loop.last %}
 {% endif -%}
 {% endfor %}
